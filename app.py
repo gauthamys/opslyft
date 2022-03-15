ea20f1b9a602d78813c1902eaa6c340f7f3a00ba
gauthamys <66833388+gauthamys@users.noreply.github.com>
2022-03-15 23:23:24 +0530

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+"""FLASK APP"""
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_opslyft():
+    """need to get a 10/10 on lint :)"""
+    return f"{os.environ.get('MESSAGE')}"
+
+if __name__ == '__main___':
+    app.run(debug=True, port=5000, host='0.0.0.0')
