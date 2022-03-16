b351d6a2f26fd112db6173ebbbfaa326f1fb4d66
gauthamys <66833388+gauthamys@users.noreply.github.com>
2022-03-16 18:38:31 +0530

    secret checking

---
+++ .github/workflows/check_secrets.py
@@ -0,0 +1,19 @@
+'''Secrets checking module for source code in python using regexes'''
+import re
+import sys
+
+files = sys.argv[1:]
+pattern = r'[a-z0-9_]*KEY=[a-zA-Z0-9]*'
+
+secrets = []
+
+for file in files:
+    with open(file) as f:
+        for lineno, line in enumerate(f.readlines()):
+            if re.search(pattern, line):
+                secrets.append((line, lineno))
+
+if len(secrets) > 0:
+    print('SECRETS FOUND: ')
+    for secret in secrets:
+        print(f'line {secret[1]}:\t{secret[0]}')
